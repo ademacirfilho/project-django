7726a2a2ba719eb8770ec8db0866406241a62723
Ademacir Filho <ademacirfilho@gmail.com>
2024-07-19 10:17:48 -0300

    primeiros comandos com o django

--- exemplo/views.py
+++ exemplo/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from datetime import datetime
 
 def index(request):
-    return render(request, "index.html")
+    lista_usuarios = [
+        {"nome": "Ademacir Filho", "idade": 17},
+        {"nome": "Lionel Messi", "idade": 37},
+        {"nome": "Wiu", "idade": 25},
+        {"nome": "Travis Scott", "idade": 29},
+    ]
+    
+    context = {
+        "usuarios": lista_usuarios,
+    }
+    return render(request, "index.html", context)
